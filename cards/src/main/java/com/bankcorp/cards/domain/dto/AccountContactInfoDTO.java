ddf4f57326b5ba89d8dfa42d01b624753e8517a0
DraganovTs <cvetomir_draganov@abv.bg>
2024-01-20 13:13:32 +0200

    spring cloud bus , actuator and webhooks

--- cards/src/main/java/com/bankcorp/cards/domain/dto/AccountContactInfoDTO.java
+++ cards/src/main/java/com/bankcorp/cards/domain/dto/AccountContactInfoDTO.java
@@ -1,12 +1,17 @@
 package com.bankcorp.cards.domain.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoDTO(String message,
-                                    Map<String,String> contactDetails,
-                                    List<String> onCallSupport) {
+public class AccountContactInfoDTO {
+    private String message;
+    private Map<String,String> contactDetails;
+    private List<String> onCallSupport;
 }
